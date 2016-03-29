f543ceb9dbb10917e9d196b52eb4d64c26e02e55
Aurélien Allienne <aurelien.allienne@gmail.com>
2016-03-29 20:08:43 +0200

    feat(configuration): Update project dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,4 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {RankMyCatApp} from './app/rank-my-cat';
-import {ROUTER_PROVIDERS} from 'angular2/router';
 
-bootstrap(RankMyCatApp, [
-  ROUTER_PROVIDERS
-]);
+bootstrap(RankMyCatApp, []);
