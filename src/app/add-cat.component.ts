b8f422eb50bc73467de4b3ff741cfe0111a9ca5f
Aurélien Allienne <aurelien.allienne@gmail.com>
2016-04-12 22:22:54 +0200

    feat(add): Add dirty checking

--- src/app/add-cat.component.ts
+++ src/app/add-cat.component.ts
@@ -1,5 +1,6 @@
 import {Component} from 'angular2/core';
 import {FormBuilder, ControlGroup, Validators} from 'angular2/common';
+import {CanDeactivate} from 'angular2/router'
 
 import {StaticValidators} from './validator/staticValidators';
 
@@ -7,7 +8,7 @@ import {StaticValidators} from './validator/staticValidators';
     selector: 'add-cat-form',
     templateUrl: 'app/add-cat.html'
 })
-export class AddCatComponent {
+export class AddCatComponent implements CanDeactivate {
     form: ControlGroup;
     
     constructor(fb: FormBuilder) {
@@ -20,5 +21,12 @@ export class AddCatComponent {
                 ])
             ]
         })
+    }
+    
+    routerCanDeactivate() {
+        if(this.form.dirty) {
+            return confirm('You have unsaved changes. Are you sure you want to navigate away?');
+        }
+        return true;
     }
 }
